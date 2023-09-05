2ba7e36c1e910f1c64d8f35b1abfcdc022c4dd72
Tiana Robinson <trobinson1097@gmail.com>
2023-09-04 23:01:48 -0700

    intial home view

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,18 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={"https://cdn.nba.com/headshots/nba/latest/1040x760/893.png"} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Historical Shot Tracking Data</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
